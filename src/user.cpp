338f681a388d5249377bb4d750219fd4bbb4fa5c
heloobaby <sun888486@qq.com>
2022-07-26 13:56:35 +0800

    add user.cpp into cache

---
+++ src/user.cpp
@@ -0,0 +1,17 @@
+/*++
+
+其他的文件不用动,只把自己的代码放到user_main函数里就行,DriverEntry中会调用
+
+--*/
+
+#include<ntifs.h>
+#include<ntddk.h>
+#include<wdm.h>
+
+#include"utils.h"
+
+
+void user_main(PDRIVER_OBJECT drv, PUNICODE_STRING reg) {
+
+}
+asd
