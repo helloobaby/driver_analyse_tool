ec2893f2e2aee822c0973007b72e4fab8bf68cf8
heloobaby <sun888486@qq.com>
2022-08-10 14:33:28 +0800

    add somethign

--- src/pe.h
+++ src/pe.h
@@ -18,7 +18,8 @@ PIMAGE_NT_HEADERS NTAPI RtlImageNtHeader(
 class pe64
 {
 public:
-	pe64(PVOID ImageBase);
+	//@isInMemory -> 表明当前是内存中的文件还是磁盘上的文件
+	pe64(PVOID ImageBase, bool isInMemory = true);
 
 	bool check_image();
 
@@ -31,6 +32,21 @@ public:
 	IMAGE_SECTION_HEADER* get_section(const char* section_name);
 	void print_sections();
 
+	using handler_type = void(*)(IMAGE_SECTION_HEADER*);
+	void processing_sections(handler_type handler);
+
+	IMAGE_DATA_DIRECTORY* get_data_dir(UINT32 id);
+
+	IMAGE_IMPORT_DESCRIPTOR* get_import_descriptor();
+	ULONG get_import_descriptor_size();
+	IMAGE_EXPORT_DIRECTORY* get_export_descriptor();
+	ULONG get_export_descriptor_size();
+
+	PVOID RVAtoP(PVOID pBase, ULONG  rva);
+	template<typename T = PVOID>
+	T get_image_base() { return (T)_image_base; }
+
 private:
 	PVOID _image_base;
+	bool _in_mem;
 };
